432c1bee9cbec014d37919fcc216307e373035d3
Lucas Silva <lucssbarrosdasilva@gmail.com>
2021-07-22 17:10:42 -0300

    feat: Adding Modular

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,36 @@
+import 'package:app_example/utils/ui_theme.dart';
 import 'package:flutter/material.dart';
 import 'utils/asset_paths.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 void main() => runApp(FutureBuilder(
-      future: Future.delayed(Duration(milliseconds: 2500)),
-      builder: (context, snapshot) =>
-          (snapshot.connectionState == ConnectionState.waiting)
-              ? MaterialApp(home: Splash())
-              : MaterialApp(home: Container(color: Colors.amberAccent)),
-    ));
+    future: Future.delayed(Duration(milliseconds: 2500)),
+    builder: (context, snapshot) =>
+        (snapshot.connectionState == ConnectionState.waiting)
+            ? MaterialApp(home: Splash())
+            : ModularApp(
+                module: AppModule(),
+                child: MaterialApp(
+                  initialRoute: '/',
+                  theme: UI.globalTheme,
+                ).modular())));
 
 class Splash extends StatelessWidget {
   @override
   build(_) => Scaffold(
-        backgroundColor: Colors.pink[400],
         body: Stack(fit: StackFit.expand, children: [
           Image.asset(ImagePaths.splashBackground, fit: BoxFit.fill),
           Center(child: Image.asset(ImagePaths.logotypeWhite)),
         ]),
       );
+}
+
+class AppModule extends Module {
+  @override
+  get binds => [];
+
+  @override
+  get routes => [
+        ChildRoute('/', child: (_, args) => Container(color: Colors.blueGrey)),
+      ];
 }
---
+++ lib/utils/ui_theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class UI {
+  UI._();
+  static ThemeData get globalTheme => ThemeData();
+}
