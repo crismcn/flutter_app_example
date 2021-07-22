c2ad02d57f9f3c7127bf0985e82ca1ae595addb1
Lucas Silva <lucssbarrosdasilva@gmail.com>
2021-07-22 01:49:15 -0300

    feat: Native Splash and a Splash Widget added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
+import 'utils/asset_paths.dart';
 
-void main() => runApp(
-      MaterialApp(
-        title: 'App Example',
-        home: Container(color: Colors.amberAccent),
-      ),
-    );
+void main() => runApp(FutureBuilder(
+      future: Future.delayed(Duration(milliseconds: 2500)),
+      builder: (context, snapshot) =>
+          (snapshot.connectionState == ConnectionState.waiting)
+              ? MaterialApp(home: Splash())
+              : MaterialApp(home: Container(color: Colors.amberAccent)),
+    ));
+
+class Splash extends StatelessWidget {
+  @override
+  build(_) => Scaffold(
+        backgroundColor: Colors.pink[400],
+        body: Stack(fit: StackFit.expand, children: [
+          Image.asset(ImagePaths.splashBackground, fit: BoxFit.fill),
+          Center(child: Image.asset(ImagePaths.logotypeWhite)),
+        ]),
+      );
+}
---
+++ lib/utils/asset_paths.dart
@@ -0,0 +1,10 @@
+class ImagePaths {
+  ImagePaths._();
+  static String get logotypeWhite => 'assets/images/logotype_white.png';
+  static String get splashBackground => 'assets/images/splash_background.png';
+}
+
+class IconPaths {
+  IconPaths._();
+  static String get logo => 'assets/icons/logo_icon.svg';
+}
